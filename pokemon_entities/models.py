dd0f3efd643efce4970c323482459fa6020a5f99
Your Name <you@example.com>
2023-04-22 22:04:07 +0300

    add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
+    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     Lat = models.FloatField()
     Lon = models.FloatField()
+
