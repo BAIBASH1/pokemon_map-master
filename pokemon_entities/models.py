ee6dfc5dc34f6023f0effb53b870c351294e1461
Your Name <you@example.com>
2023-04-22 21:20:36 +0300

    add Pokemon Entity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,10 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True)
 
-
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    Lat = models.FloatField()
+    Lon = models.FloatField()
