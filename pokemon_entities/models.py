92e1f9905c2cd7a99e183c059bc5b6d20976d528
unknown <gazinyr2014@mail.ru>
2023-04-28 04:07:00 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -20,7 +20,9 @@ class Pokemon(models.Model):
     image = models.ImageField(
         blank=True,
         verbose_name="фотография",
-        default="no_image.png"
+        default='https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
+                '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
+                '&fill=transparent'
     )
 
     description = models.TextField(
